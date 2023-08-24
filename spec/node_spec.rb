31a8eb74739b2a2739bbeb7894b662552cfcf4d5
Scott DeVoss <scottdevoss@yahoo.com>
2023-08-23 19:01:31 -0600

    Initial commit

---
+++ spec/node_spec.rb
@@ -0,0 +1,13 @@
+require './lib/node'
+require 'rspec'
+
+RSpec.describe Node do
+  describe '#initialize' do
+
+    it 'is an instance of node' do
+      node = Node.new("plop")
+      expect(node).to be_instance_of(Node)
+      
+    end
+  end
+end
