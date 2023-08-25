7da9f8168c579b57b948a64441060734007ef771
Scott DeVoss <scottdevoss@yahoo.com>
2023-08-25 17:40:32 -0600

    add tests to pass first 2 blocks of iteration 1

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,17 +1,56 @@
 class LinkedList 
-  attr_reader :head, :data, :next_node
+  attr_reader :head
   def initialize
     @head = nil
-    @data 
-    @next_node
-
   end
 
-  def append(sound)
-    if (@data)
-      @data.append(sound)
-    else @data = sound
-    end
+  def append(data)
+    new_node = Node.new(data)
+    
+    if @head.nil?
+      @head = Node.new(data)
+    else 
+      last_node = @head
+      
+      # until last_node.next_node.nil?
+      #   last_node = lastNode.next_node
+      # end
+      #We are at the end of the list
+      last_node.next_node = Node.new(data)
+      # require 'pry'; binding.pry
+    end 
+  
   end
 
-end
+  def count 
+    count = 0
+    count += 1
+  end
+
+  def to_string
+  # start with empty string
+    string = ""
+    if @head.nil?
+      string
+    else
+      string = string + @head.data #"doop"
+
+      #iterate until we get nil
+      #add the nodes data to the string
+    end 
+    string
+  end
+  
+end 
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,23 +1,41 @@
 require "./lib/linked_list"
 require './lib/node'
 require 'rspec'
+require 'pry'
 
 RSpec.describe LinkedList do
   describe '#initialize' do
 
     it 'is an instance of linked list' do
       list = LinkedList.new
+      list.append("doop")
       expect(list).to be_instance_of(LinkedList)
     end
 
     it 'has a head' do
       list = LinkedList.new
+      #list.append("doop")
       expect(list.head).to be_nil
     end
 
-    it 'adds a new piece of data to the list' do
+    it 'add an element' do
       list = LinkedList.new
-      expect(list.append("doop")).to eq ("doop")
+      list.append("doop")
+      #list.append("deep")
+      # require 'pry'; binding.pry
+      expect(list).to be_an_instance_of(LinkedList)
+    end
+
+    it 'counts the nodes' do
+      list = LinkedList.new
+      list.append("doop")
+      expect(list.count).to eq(1)
+    end
+
+    it 'turns data to string' do
+      list = LinkedList.new
+      list.append("doop")
+      expect(list.to_string).to eq("doop")
     end
   end
 end
