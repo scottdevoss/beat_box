8c6b7bcb8d3cf408c9b528a9038dc4cb8b0e3f29
Scott DeVoss <scottdevoss@yahoo.com>
2023-08-27 13:50:13 -0600

    made changes to append  method and to string method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,6 @@
+require './lib/node'
+require 'pry'
+
 class LinkedList 
   attr_reader :head
   def initialize
@@ -6,19 +9,25 @@ class LinkedList
 
   def append(data)
     new_node = Node.new(data)
-    
-    if @head.nil?
-      @head = Node.new(data)
-    else 
-      last_node = @head
-      
-      until last_node.next_node.nil?
-        last_node = last_node.next_node
+    if @head == nil
+      @head = new_node
+    else
+      current_node = @head
+      until current_node.next_node == nil
+        current_node = current_node.next_node
       end
-      #We are at the end of the list
-      last_node.next_node = Node.new(data)
+      current_node.next_node = new_node
     end 
-    #require 'pry'; binding.pry
+    # if @head.nil?
+    #   @head = Node.new(data)
+    # else 
+    #   last_node = @head
+      
+    #   until last_node.next_node.nil?
+    #     last_node = last_node.next_node
+    #   end
+    #   #We are at the end of the list
+    #   last_node.next_node = Node.new(data)
   
   end
 
@@ -61,17 +70,27 @@ class LinkedList
   end
 
   def to_string
-    # start with empty string
-      string = ""
-      current_node = @head
-      while current_node 
-        string << current_node.data + " "
-        current_node = current_node.next_node
-  
-        #add the nodes data to the string
-      end 
-      string
+    string_node = @head
+    string_words = []
+    while string_node
+      string_data = string_node.data.to_s
+      string_words << string_data
+      string_node = string_node.next_node
     end
+    string_words.join(" ")
+
+    # # start with empty string
+    #   string = ""
+    #   current_node = @head
+    #   while current_node 
+    #     string << current_node.data + " "
+    #     current_node = current_node.next_node
+  
+    #     #add the nodes data to the string
+    #   end 
+    #   string
+    end
+
 
     def find(start_position, count)
       return "" if start_position < 0 || count <= 0
@@ -102,28 +121,25 @@ class LinkedList
     end
 
     def pop
-      
+      node = @head
+      return nil if node.nil?
+
+      if node.next_node == nil
+        next_node
+      else
+        until node.next_node.nil?
+          node = node.next_node
+        end
+        node.data
+      end
     end
 
 end 
 
+# binding.pry
 
 
 
-# def to_string
-#   # start with empty string
-#     return "" if @head == nil
-#     return @head.data if @head.next_node == nil
-
-#     the_beat = @head.data #doop
-#     current_node = @head.next_node #node instance with data of deep
-
-#     while current_node.next_node != nil do
-#       the_beat << " " + current_node.data
-#       current_node = current_node.next_node
-#     end
-#     the_beat << " " + current_node.data
-#   end
 
 
 
