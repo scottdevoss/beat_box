edf4467d06c71a227e8d46c3e57ada2bbe94c183
Scott DeVoss <scottdevoss@yahoo.com>
2023-08-29 08:10:50 -0600

    fixed spacing in each file

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,7 +3,6 @@ require 'rspec'
 
 RSpec.describe Node do
   describe '#initialize' do
-
     it 'is an instance of node' do
       node = Node.new("plop")
       expect(node).to be_instance_of(Node)
